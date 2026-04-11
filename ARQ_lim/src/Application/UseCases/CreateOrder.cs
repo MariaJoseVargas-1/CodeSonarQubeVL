6e15b541fab0a51c3673e6d99a8a247065c0986b
Majo <mjsilva890@misena.edu.co>
2026-04-11 00:06:04 -0500

    error con correcion que sonar no detecta

--- ARQ_lim/src/Application/UseCases/CreateOrder.cs
+++ ARQ_lim/src/Application/UseCases/CreateOrder.cs
@@ -9,13 +9,13 @@ using Infrastructure.Logging;
 
 public class CreateOrderUseCase
 {
-    public Order Execute(string customer, string product, int qty, decimal price)
+    public static Order Execute(string customer, string product, int qty, decimal price)
     {
         Logger.Log("CreateOrderUseCase starting");
         var order = OrderService.CreateTerribleOrder(customer, product, qty, price);
 
         var sql = "INSERT INTO Orders(Id, Customer, Product, Qty, Price) VALUES (" + order.Id + ", '" + customer + "', '" + product + "', " + qty + ", " + price + ")";
-        Logger.Try(() => BadDb.ExecuteNonQueryUnsafe(sql)); // swallow failures silently
+        Logger.Try(() => BadDb.ExecuteNonQueryUnsafe(sql)); 
 
         System.Threading.Thread.Sleep(1500);
 
