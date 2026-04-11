199b52b92a036d3bac7c6871324324c6ec3ce993
Majo <mjsilva890@misena.edu.co>
2026-04-11 00:03:05 -0500

    Se arreglan errores que marca sonar con metodos y uns constante

--- ARQ_lim/src/WebApi/Controllers/OrdersController.cs
+++ ARQ_lim/src/WebApi/Controllers/OrdersController.cs
@@ -3,8 +3,8 @@ using System;
 
 namespace WebApi.Controllers
 {
-    public class OrdersController
+    public static class OrdersController
     {
-        public static string DoNothing() => "This controller does nothing. Endpoints are in Program.cs";
+        public const string Message => "This controller does nothing. Endpoints are in Program.cs";
     }
 }
