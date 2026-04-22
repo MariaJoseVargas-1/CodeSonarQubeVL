6dded9b22eff206f8544a3734c87884b105884ce
Majo <mjsilva890@misena.edu.co>
2026-04-21 22:43:50 -0500

    se cambio OrderService, por error detectado en Sonar nuevamente

--- ARQ_lim/src/Domain/Services/OrderService.cs
+++ ARQ_lim/src/Domain/Services/OrderService.cs
@@ -7,23 +7,24 @@ using Domain.Entities;
 
 public static class OrderService
 {
-
     private static readonly List<Order> _lastOrders = new List<Order>();
 
     public static IReadOnlyList<Order> LastOrders => _lastOrders;
+
+    private static readonly Random _random = new Random();
 
     public static Order CreateTerribleOrder(string customer, string product, int qty, decimal price)
     {
         var o = new Order
         {
-            Id = new Random().Next(1, 9999999),
+            Id = _random.Next(1, 9999999),
             CustomerName = customer,
             ProductName = product,
             Quantity = qty,
             UnitPrice = price
         };
 
-        _lastOrders.Add(o); // misma lógica, mismos datos
+        _lastOrders.Add(o);
         return o;
     }
 }
